52e4ab6871080da139dd493f0770ce4e603b4f70
jcmq6b <60238224+jcmq6b@users.noreply.github.com>
2021-03-28 07:58:13 -0500

    added scroll to top function for projects page

--- PortfolioWebsite/src/app/pages/projects/projects.component.ts
+++ PortfolioWebsite/src/app/pages/projects/projects.component.ts
@@ -18,16 +18,13 @@ export class ProjectsComponent implements OnInit {
       if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
           this.windowScrolled = true;
       } 
-     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
-          this.windowScrolled = false;
-      }
   }
   scrollToTop() {
       (function smoothscroll() {
           var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
           if (currentScroll > 0) {
-              window.requestAnimationFrame(smoothscroll);
-              window.scrollTo(0, currentScroll - (currentScroll / 8));
+              window.requestAnimationFrame(smoothscroll); //Animation
+              window.scrollTo(0, currentScroll - (currentScroll / 8)); //Where to scroll (to, from)
           }
       })();
   }
