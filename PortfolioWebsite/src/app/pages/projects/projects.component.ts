415d57c1820b8892317435fd2ef76673156ea1f0
jcmq6b <60238224+jcmq6b@users.noreply.github.com>
2021-03-28 07:45:02 -0500

    finished updating UI for mobile

--- PortfolioWebsite/src/app/pages/projects/projects.component.ts
+++ PortfolioWebsite/src/app/pages/projects/projects.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
+
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  windowScrolled: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } 
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop() {
+      (function smoothscroll() {
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+      })();
+  }
 
   ngOnInit(): void {
   }
-
 }
+
+
